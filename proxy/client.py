d88137c7e222b1fbd4c3cc5aadf27a7050979678
BANN42 <151378495+BANN42@users.noreply.github.com>
2026-07-07 00:47:32 +0200

    Just Brain Storming NeverMind

---
+++ proxy/client.py
@@ -0,0 +1,8 @@
+import socket as sk
+import threading as thread
+
+class Client:
+     def __init__(self, clientProper):
+          self.clientProper= clientProper
+     def makeRequest(self, fromm , too):
+          pass
--- proxy/server.py
+++ proxy/server.py
@@ -1,1 +1,28 @@
-import threading
+import socket as sk
+
+# V1 request description
+class Req:
+     def __init__(self, srcIp ,dstIp, header):
+          self.srcIp = srcIp
+          self.dstIp = dstIp
+          self.header = header
+
+class Server_R:
+     def __init__(self ,host, reqSrc , reqDst):
+          self.host = host
+          self.reqSrc =reqSrc
+          self.reqDst = reqDst
+
+     # establish connection to the server
+     def establishConnection(self):pass
+     # receive request
+     def receiveRequest(self):pass
+     # redirect Request
+     def redirectReq(self, ClientReq ):pass
+     # filter Request
+     def filterReq(self,clientReq) :pass
+     # prevent Request
+     def preventReq(self, clientReq): pass
+     # analyzer Request
+     def analyzerReq(self , clientReq) : pass
+     
